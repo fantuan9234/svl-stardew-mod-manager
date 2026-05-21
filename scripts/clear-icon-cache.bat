2fedb544c530ed58b5202c1b4539bf7d0d1883df
fantuan9234 <18138844056@163.com>
2026-05-21 20:56:58 +0800

    clean: remove unrelated files, keep only SVL project source

--- scripts/clear-icon-cache.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-taskkill /f /im explorer.exe
-del /f /s /q %localappdata%\IconCache.db
-del /f /s /q %localappdata%\Microsoft\Windows\Explorer\iconcache_*.db
-del /f /s /q %localappdata%\Microsoft\Windows\Explorer\thumbcache_*.db
-start explorer.exe
