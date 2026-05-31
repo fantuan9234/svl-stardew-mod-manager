9e429ac3622e562e1b2b05b9d4c37633e8550d53
fantuan9234 <18138844056@163.com>
2026-05-31 21:04:03 +0800

    release v1.2.7

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -69,6 +69,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
 
         .setup(|app| {
             let handle = app.handle().clone();
